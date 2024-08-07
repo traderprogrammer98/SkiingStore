49ebdda4cb9878ebe3a927099c9ef3ce956f7a92
jamshidsikim1 <139772090+jamshidsikim1@users.noreply.github.com>
2024-08-07 03:48:49 -0700

    entering cart

--- SkiingStore/DTOs/BasketDto.cs
+++ SkiingStore/DTOs/BasketDto.cs
@@ -1,9 +1,6 @@
-﻿namespace SkiingStore.DTOs
+﻿namespace SkiingStore.Dtos
 {
     public class BasketDto
     {
-        public int Id { get; set; }
-        public string BuyerId { get; set; }
-        public List<BasketItemDto> BasketItems { get; set; }
     }
 }
